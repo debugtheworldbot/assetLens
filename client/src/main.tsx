d7084102c6559ed824933690183c4db29e17f944
pipizhu <istiancz@gmail.com>
2026-06-22 14:58:39 +0800

    Guard analytics script envs

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,6 +6,17 @@ import superjson from "superjson";
 import App from "./App";
 import "./index.css";
 
+const analyticsEndpoint = import.meta.env.VITE_ANALYTICS_ENDPOINT;
+const analyticsWebsiteId = import.meta.env.VITE_ANALYTICS_WEBSITE_ID;
+
+if (analyticsEndpoint && analyticsWebsiteId) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = `${analyticsEndpoint.replace(/\/$/, "")}/umami`;
+  script.dataset.websiteId = analyticsWebsiteId;
+  document.body.append(script);
+}
+
 const queryClient = new QueryClient();
 
 queryClient.getQueryCache().subscribe(event => {
